13fba0fb388e166736a97dc665fbf178c79c1439
LReyna <leo.reyna@gmail.com>
2023-06-25 22:48:22 -0400

    Added YTD Total Revenue

--- NorthwindTraders.sql
+++ NorthwindTraders.sql
@@ -1,7 +1,26 @@
 -- Maven Analytics Nortwind Traders Challenge
 -- Top Level Analysis
 
--- Count of Orders
+USE [Northwind Traders]
+
+/* Count of Orders
+There a total of 830 Products
+*/
 SELECT COUNT(OrderID) as [Order Count]
 FROM dbo.orders;
 
+
+/* YTD Total Revenue */
+SELECT FORMAT(sum(quantity * unitPrice),'C') AS [YTD Total Sales]
+FROM dbo.order_details;
+
+/* TOP 5 PRODUCTS 
+The number 1 product is Camember Pierrot with 1,577, 2nd Place Raclette Couravault with 1,496 and 3rd  place to Gorgonzola Telino with 1,397 pieces, 4th place goes to Gnocchi di nonna Alice at 1,263 and on 5th place is Pavlova with 1,158
+*/
+SELECT TOP 5
+    prd.productName AS [Product Name], FORMAT(CAST(SUM(ord.quantity) AS decimal (10,1)), '0,0') AS [Qty Sold]
+FROM dbo.order_details AS ord
+    INNER JOIN dbo.products AS prd
+    ON ord.productID = prd.productID
+GROUP BY prd.productName
+ORDER BY SUM(ord.quantity) DESC;
