16dc90bab36a98cbed07a1926b975a8d07fcfd9d
LReyna <leo.reyna@gmail.com>
2023-06-27 19:32:01 -0400

    fixing issues

--- NorthwindTraders.sql
+++ NorthwindTraders.sql
@@ -21,7 +21,10 @@ SELECT FORMAT(sum(quantity * unitPrice *(1- discount)),'C') AS [YTD Net Sales]
 FROM dbo.order_details;
 
 /* TOP 5 PRODUCTS 
-The number 1 product is Camember Pierrot with 1,577, 2nd Place Raclette Couravault with 1,496 and 3rd  place to Gorgonzola Telino with 1,397 pieces, 4th place goes to Gnocchi di nonna Alice at 1,263 and on 5th place is Pavlova with 1,158
+The number 1 product is Camember Pierrot with 1,577, 
+2nd Place Raclette Couravault with 1,496  and 3rd place 
+to Gorgonzola Telino with 1,397 pieces, 4th place goes 
+to Gnocchi di nonna Alice at 1,263 and on 5th place is Pavlova with 1,158
 */
 SELECT TOP 5
     prd.productName AS [Product Name], FORMAT(CAST(SUM(ord.quantity) AS decimal (10,1)), '0,0') AS [Qty Sold]
