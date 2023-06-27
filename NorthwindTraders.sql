34162a21ba22f2855414ecada703fdd4c7a31970
LReyna <leo.reyna@gmail.com>
2023-06-26 21:16:53 -0400

    commit6

--- NorthwindTraders.sql
+++ NorthwindTraders.sql
@@ -3,17 +3,21 @@
 
 USE [Northwind Traders]
 
-/* Count of Orders
+/* SALES 
+Count of Orders
 There a total of 830 Products
 */
-SELECT COUNT(OrderID) as [Order Count]
-FROM dbo.orders;
-
 
 /* YTD Total Revenue 
-YTD Total Sales were $1.3 Million
+YTD Total Sales were $1.354,458.59 Million
 */
 SELECT FORMAT(sum(quantity * unitPrice),'C') AS [YTD Total Sales]
+FROM dbo.order_details;
+
+/* YTD Net Revenue 
+YTD Total Sales were $1,265,793.04 Million
+*/
+SELECT FORMAT(sum(quantity * unitPrice *(1- discount)),'C') AS [YTD Net Sales]
 FROM dbo.order_details;
 
 /* TOP 5 PRODUCTS 
@@ -22,11 +26,8 @@ The number 1 product is Camember Pierrot with 1,577, 2nd Place Raclette Couravau
 SELECT TOP 5
     prd.productName AS [Product Name], FORMAT(CAST(SUM(ord.quantity) AS decimal (10,1)), '0,0') AS [Qty Sold]
 FROM dbo.order_details AS ord
-    INNER JOIN dbo.products AS prd
+INNER JOIN dbo.products AS prd
     ON ord.productID = prd.productID
 GROUP BY prd.productName
 ORDER BY SUM(ord.quantity) DESC;
 
-USE [Northwind Traders]
-SELECT *
-FROM dbo.Employees
